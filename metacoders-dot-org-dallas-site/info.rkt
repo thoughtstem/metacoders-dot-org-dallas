df5863349b17f0e5de661649132a3c28b0dccf2d
aBlender <JasonLongLe@gmail.com>
2019-11-21 18:58:16 -0800

    Initial commit

---
+++ metacoders-dot-org-dallas-site/info.rkt
@@ -0,0 +1,4 @@
+#lang info
+(define collection    "metacoders-dot-org-dallas-site")
+(define deps '("base" 
+               "https://github.com/thoughtstem/metacoders-dot-org-dallas.git?path=metacoders-dot-org-dallas-lib"))
