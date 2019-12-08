b65c37002d8b74911b3bbd80a0ef0f6413a9c1db
Lindsey Handley, Ph.D <lindseyhandley@gmail.com>
2019-12-08 11:55:51 -0800

    add city-name

--- metacoders-dot-org-dallas-site/main.rkt
+++ metacoders-dot-org-dallas-site/main.rkt
@@ -25,6 +25,7 @@
 (define (index)
   (page index.html
         (city-page
+         #:city-name "Dallas"
          #:banner-url "https://live.staticflickr.com/3937/33074076002_b2f829bc2d_k.jpg"
          #:locations-list (list
                            (location-courses
