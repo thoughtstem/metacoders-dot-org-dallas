e7f4e9e51c1f796a9a7ad0ac5fa7f57d44fad123
Lindsey Handley, Ph.D <lindseyhandley@gmail.com>
2020-06-03 14:58:12 -0700

    removing dynamic content

--- metacoders-dot-org-dallas-site/main.rkt
+++ metacoders-dot-org-dallas-site/main.rkt
@@ -14,8 +14,9 @@
          #:banner-url (prefix/pathify dallas-banner-path)
          #:alt-tag "City of Dallas, Texas with a view of downtown on a cloudy day"
 
-	 (remote 
-	    "https://mc-social-media.s3-us-west-1.amazonaws.com/static-points-of-sale/dallas.html"))))
+	 (div class: "mb-0 pt-5 pb-5 text-center"
+	 	(h2 "Coming Soon!")
+	 	(p "We're prioritizing students', employees', and families' health during COVID-19. In the meantime, check out our Quarantine Coding Club using the link above!")))))
 
 
 (define (pages)
